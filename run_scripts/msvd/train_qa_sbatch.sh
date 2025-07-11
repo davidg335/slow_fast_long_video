282e70eb40ce12f14df373f7d337890ada00e385
David Gardner <davidg3@bu.edu>
2025-07-11 16:26:34 -0400

    small sbatch change

--- run_scripts/msvd/train_qa_sbatch.sh
+++ run_scripts/msvd/train_qa_sbatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -c 8                                # Number of CPU cores
-#SBATCH --gres=gpu:2                                # Request 2 GPUs (any type for now)
+#SBATCH --gpus=2                                # Request 2 GPUs (any type for now)
 #SBATCH -C 'gmem24|gmem32|gmemT48|gmem48|gmem80'    # GPU memory constraint (matches -C)
 #SBATCH --job-name=train_msvd_qa             # Optional job name
 #SBATCH --output=logs/train/msvd_test_qa_%j.out        # STDOUT + STDERR to file (useful for debugging)
